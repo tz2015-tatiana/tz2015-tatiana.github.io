0e46080ca889364a37dd06c774227e49f75548fb
tz2015-tatiana <tz2015@hw.ac.uk>
2023-11-03 09:51:39 +0400

    finish first task

--- week4/README.md
+++ week4/README.md
@@ -1,17 +1,17 @@
-# Travel Agency web site
+# Cities web site
 
-Educational project based on a multipage website template.
+Educational project.
 
 ## Description
 
-The purpose of this project is to practice with Git, GitHub, Pages and Jekyll.
-During week 3 lab the slide show was added to the gallery page using JS. Also, after clicking
-the button "Register Now" popup with registration form appear.
+The purpose of this project is to practice with JSON and AJAX.
+During week 4 lab we understand the fundamentals of JSON and AJAX, and how we can utilize
+these technologies to dynamically load data into our web pages without requiring a page refresh.
 
 ## Authors
 
-The website template is provided by Professor Salma Smaeer Zalkha (https://github.com/Salma-S-HW/F28WP-lab1.git).
+The website template is provided by Professor Salma Smaeer Zalkha.
 
 ### Web link
-To see the week 3 final result, please click:
-http://tz2015-tatiana.github.io/week3
+To see the week 4 final result, please click:
+http://tz2015-tatiana.github.io/week4
--- week4/main.js
+++ week4/main.js
@@ -1,14 +1,45 @@
-const myCity = {
-  "name": "Dubai",
-  "country": "UAE",
-  "place": "Dubai Mall"
+const cityContainer = document.getElementById("city-info");
+const btn = document.querySelector(".button");
+
+const renderHTML = function(data) {
+  let htmlString = "";
+
+  for (i=0; i<data.length; i++) {
+    htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ",</br>Where you can enjoy indoor places like: ";
+    for (ii = 0; ii < data[i].places.indoor.length; ii++) {
+    // Loop through the indoor places of the current city.
+      if (ii == 0) {
+      htmlString += data[i].places.indoor[ii];
+      } else {
+      htmlString += ", and " + data[i].places.indoor[ii];
+      }
+    }
+    htmlString += '. & enjoy outdoor places like: ';
+    // Loop through the outdoor places of the current city.
+    for (ii = 0; ii < data[i].places.outdoor.length; ii++) {
+      if (ii == 0) {
+      htmlString += data[i].places.outdoor[ii];
+      } else {
+      htmlString += " and " + data[i].places.outdoor[ii];
+      }
+    }
+    htmlString += '.</p>';
+  }  
+  cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+  btn.classList.add("hide-me");
 }
 
-const ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://tz2015-tatiana.github.io/week4/cities1.json');
+const getData = function() {
+  const ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://tz2015-tatiana.github.io/week4/cities1.json');
 
-ourRequest.onload = function() {
-  console.log(ourRequest.responseText);
-};
+  ourRequest.onload = function() {
+    let ourData = JSON.parse(ourRequest.responseText);
+    //console.log(ourData[0]);
+    renderHTML(ourData);
+  };
 
-ourRequest.send();
+  ourRequest.send();
+}
+
+btn.addEventListener("click", getData);
